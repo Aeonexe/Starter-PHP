e4d08699cf82345cbbc479f5ada42aa3bbf7192e
Albrt <me@albertohartzet.com>
2017-06-30 23:12:49 -0500

    Añadido /fonts
    Open sans
    Font awesome

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
-<?php include( 'header.php' ); ?>
+<?php
+
+$headers = array(
+	'html_class' 	=> 'page',
+	'body_class' 	=> 'home',
+	'title'		=> 'Home Pages',
+	'description'	=> 'Descripción de la página para meta etiqueta de descripción',
+	'author'		=> 'Autor del sitio',
+	'itemprop_name'	=> 'Título para schema.org',
+	'itemprop_desc' 	=> 'Descripción para schema.org',
+	'itemprop_image'	=> 'img/og-facebook.png',
+	'creation_date' 	=> '',
+	'revised_date'	=> ''
+);
+
+include( 'header.php' ); ?>
 
 			<p>Content</p>
 			<button class="uk-button">click</button>
-			<?php
-			page_url();
-			?>
+			<?php page_url(); ?>
 
 <?php include( 'footer.php' ); ?>
